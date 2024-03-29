7e771bd9cbd36b438ed61b0324365b50721c8a01
unknown <parmarparas3321@gmail.com>
2024-03-29 08:28:26 +0530

    update resume

---
+++ Resume/App.js
@@ -0,0 +1,14 @@
+
+import Resume from './Resume';
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <Resume/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ Resume/Resume.js
@@ -0,0 +1,98 @@
+
+function Resume() {
+
+    return(
+
+<>
+<div className="resume">
+      <div className="header" align="center">
+        <h1>JONATHAN WRIGHT</h1>
+        <p>Lead Business Analyst | +1-921-389-0182 | name@gmail.com | <a href="https://linkedin.com/in/jonwrightjr">LinkedIn</a></p>
+        <p>Location: Dallas, TX</p>
+      </div>
+<div>
+    <hr></hr>
+</div>
+      <div className="summary">
+        <h2 align="center">Summary</h2>
+        <p>I am an experienced Business Analyst with a strong technical background and great project management skills. I am able to work on multiple projects at the same time, which over the years at Network Solutions have lead to eight-digit efficiencies for the business. Furthermore, I immensely enjoy the process of deriving critical business insights using analytics, and then supporting or directly managing the efficiency implementation projects. My ability to move from analysis or leading a team of analysts to supporting or leading the actual project is something that could help further accelerate the velocity of work at lon Insights.</p>
+      </div>
+        
+
+      <div className="skills">
+        <h2 align="center">Skills</h2>
+        <hr/>
+        <div className="skill">
+         
+          <p> Business : Budgeting, Financial Analysis, Project Management, Stakeholder Management, Business Strategy</p>
+        </div>
+        <div className="skill">
+          
+          <p> Technology  : Excel, VBA, SQL, QuickBooks, Power BI, Tableau, Python</p>
+        </div>
+      </div>
+     
+      <div className="experience">
+        <h2 align="center">Experience</h2>
+        <hr/>
+        <div className="job">
+          <h3>Lead Business Analyst & Project Manager, Network Solutions LLC</h3>
+          <ul>
+            <li>Created new strategies to manage $2 million of accounts at risk, resulting in an increase of 4% in revenue in 6 months.</li>
+            <li>Led the effort to deploy an automated time & expense reporting system for more than 90 onsite and offsite personnel across 3 locations.</li>
+            <li>Oversaw the budget and schedule of a project to recruit, hire, and train 150 new employees at five new locations.</li>
+          </ul>
+        </div>
+        <div className="job">
+          <h3>Senior Business Analyst, Network Solutions LLC</h3>
+          <ul>
+            <li>Through an improved pricing model, increased gross revenue of 15% in 2018 compared to 2017 with no change to fixed costs.</li>
+            <li>Reduced warehouse processing time by 30% in just 3 months while industry norm is 10 months.</li>
+            <li>Achieved project milestones and deliverables with an internal and external team of 10+ analysts.</li>
+          </ul>
+        </div>
+        <div className="job">
+          <h3>Business Analyst, Lauzon</h3>
+          <ul>
+            <li>Prepared 2016 Budget with Variance analysis to prior years.</li>
+            <li>Assisted merger in advanced electronics space, identifying synergy opportunities of $60M.</li>
+            <li>Reduced IPS (Customer Issues) by 2.2% while impacting merely 3% of GMV.</li>
+            <li>Designed and maintained 10+ data integration jobs.</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="education">
+        <h2 align="center">Education</h2> 
+        <hr/>
+        <ul>
+          <li>M.Sc. in Finance, University of Wisconsin</li>
+          <li>BBA: Business, Supply Chain Management, University of Wisconsin</li>
+        </ul>
+      </div>
+
+      <div className="certifications">
+        <h2 align="center">Certifications</h2>
+        <hr/>
+        <ul>
+          <li>PMI Professional in Business Analysis (PBA), PMI, 2019</li>
+          <li>Certified Associate in Project Management (CAPM), PMI, 2018</li>
+          <li>High-Dimensional Data Analysis, Harvard, 2017</li>
+        </ul>
+      </div>
+
+      <div className="interests">
+        <h2 align="center">Interests & Hobbies</h2>
+        <hr/>
+        <ul>
+          <li>Horse-riding & spending time in nature</li>
+          <li>Recharging during the weekend is vital for Leading a high-performing team</li>
+          <li>Developing my team into star analysts - Not only is it very satisfying, but it is also the highest value-add of any leader.</li>
+          <li>Giving back to my community - With my two kids, I spend at least one day each month volunteering.</li>
+        </ul>
+      </div>
+    </div>
+</>
+    );
+}
+export default Resume;
